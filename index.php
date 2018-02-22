6695989eb69cbce1fd0124fff00ffbd0a742bc76
Kel10IR <indo.lalisa@gmail.com>
2018-02-23 00:01:17 +0700

    tugas 3 IR

---
+++ index.php
@@ -0,0 +1,26 @@
+<html>
+ 
+<head>
+ 
+</head>
+ 
+<body>
+<center> 
+
+<p style="font-size:24px;">Pilih File PDF yang akan di Upload</p>
+<br />
+ 
+<form action="upload_file.php" method="post" enctype="multipart/form-data">
+ 
+<input type="file" name="file" size="50" />
+ 
+<br />
+<br />
+ 
+<input type="submit" value="Upload" />
+ 
+</form>
+</center> 
+</body>
+ 
+</html>
